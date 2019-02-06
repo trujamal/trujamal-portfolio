9efff05bfbb0d1e2a5771ca30423c596c6ad58d1
trujamal <16698402+trujamal@users.noreply.github.com>
2019-02-06 17:11:58 -0600

    Initial Gatsby Build

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Trujamal" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Visual Exploration</h1>
+    <p>Pushing the boundareis of photography</p>
+    <p>by taking abstract images</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
