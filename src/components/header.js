c0ad299128331e31fb8b546ec40a7ff2b3ec7599
trujamal <16698402+trujamal@users.noreply.github.com>
2019-02-08 13:04:08 -0600

    Updates to stylized component library

--- src/components/header.js
+++ src/components/header.js
@@ -3,24 +3,48 @@ import PropTypes from "prop-types"
 import React from "react"
 import './Header.css'
 
-const Header = ({ siteTitle }) => (
- 	<div className="Header">
- 		<div className="HeaderGroup">
- 			<Link to="/">Trujamal</Link>
- 			<Link to="/Work">Work</Link>
- 			<Link to="/About">About</Link>
- 			<Link to="/Contact">Contact</Link>
- 			<Link to="/Portfolio">Porfolio</Link>
- 		</div>
- 	</div>
-)
-
 Header.propTypes = {
   siteTitle: PropTypes.string,
 }
 
 Header.defaultProps = {
   siteTitle: ``,
+}
+
+class Header extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			hasScrolled: false
+		}
+	}
+
+	componentDidMount() {
+		window.addEventListener('scroll',this.handleScroll)
+	}
+
+	handleScroll = (event) => {
+		const scrollTop = window.pageYOffset
+
+		if(scrollTop > 50) {
+			this.setState({hasScrolled: true})
+		}else{
+			this.setState({hasScrolled: false})
+		}
+	}
+	render() {
+		return(
+		 	<div className={this.state.hasScrolled ? 'Header HeaderScrollled' : 'Header'}>
+		 		<div className="HeaderGroup">
+		 			<Link to="/">Trujamal</Link>
+		 			<Link to="/Work">Work</Link>
+		 			<Link to="/About">About</Link>
+		 			<Link to="/Contact">Contact</Link>
+		 			<Link to="/Portfolio">Porfolio</Link>
+		 		</div>
+		 	</div>
+ 		)
+	}
 }
 
 export default Header
