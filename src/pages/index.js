98a6ce40d4e48eaeabb297250a99d9efa37481d7
trujamal <16698402+trujamal@users.noreply.github.com>
2019-02-07 22:50:39 -0600

    Minor css animation updates.

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,14 @@ const IndexPage = () => (
     <div className="Onboarding">
       <div className="OnboardingGroup">
         <h1>creative</h1>
-        <h2>Shooting eye-catching moments for fun</h2>
+        <h2>Shooting eye-catching<br />moments for fun</h2>
         <p>Every image is a story in frame. Discover my most popular images and uncover the stories behind them.</p>
         <Link to="/page-2/">Discover my work</Link>
+        <svg width="2560" height="172" viewBox="0 0 2560 172" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z" fill="white">
+          <animate repeatCount="indefinite" fill="freeze" attributeName="d" dur="10s" values="" />
+          </path> 
+        </svg>
       </div>
     </div>
   </Layout>
