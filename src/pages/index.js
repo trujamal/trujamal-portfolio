71637570806196e07e2f34c861c35a23079d1309
trujamal <16698402+trujamal@users.noreply.github.com>
2019-02-08 20:47:26 -0600

    Creation of main page cells.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,30 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Section from '../components/Section'
 import Wave from '../components/wave'
+import staticdata from '../../staticdata.json'
+import Cell from '../components/Cell'
+import styled from 'styled-components'
+
+const SectionCaption = styled.p`
+  font-weight: 600;
+  font-size: 18px;
+  text-transform: uppercase;
+  color:#FF6541;
+  text-align: center;
+`
+
+const SectionCellGroup = styled.div`
+  max-width: 800px;
+  margin: 0 auto 100px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-column-gap: 20px;
+  padding: 0 20px;
+
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`
 
 const IndexPage = () => (
   <Layout>
@@ -48,6 +72,14 @@ const IndexPage = () => (
       people’s ideas and visions with creative experience, then crafting a product they want and to see come alive in the digital space. As a critical thinker and a problem solver, I love any challenge
       that I’m presented with and will go above and beyond whenever the opportunity arises."
     />
+    <SectionCaption>A couple of my works.</SectionCaption>
+    <SectionCellGroup>
+    {staticdata.cells.map(cell => (
+            <Cell 
+              title={cell.title}
+              image={cell.image}/>
+          ))}
+    </SectionCellGroup>
   </Layout>
 )
 
