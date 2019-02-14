449f470f65ebb682feed011dfd7149b8bf762744
trujamal <16698402+trujamal@users.noreply.github.com>
2019-02-13 19:15:52 -0600

    Minor changes to footer component, and other additions to pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,42 +2,40 @@ import React from 'react'
 import styled from 'styled-components'
 
 const FooterGroup = styled.div`
-	background: #F1F3F5;
-	padding: 50px 0;
-	display: grid;
-	grid-gap: 20px;
+	background: #1A1B1C;
+    padding: 50px 0;
+    grid-gap: 20px;
+    height: 100vh;
+    margin: 0 auto;
 `
 
 const Text = styled.p`
 	font-size: 24px;
 	font-weight: 600;
-	color: #142832;
+	color: #F5FFFF;
 	max-width: 500px;
 	margin: 0 auto;
 `
+const Saying = styled.div`
 
-const SecondaryText = styled.p`
-	font-size: 28px;
-	margin: 0 auto;
 
 `
 
-const Button = styled.button`
-	font-size: 24px;
-    color: white;
-    border: none;
-    padding: 16px 60px;
-    font-weight: 600;
-    background: linear-gradient(102.24deg, #9B51E0 0%, #3436E7 100%);
-    box-shadow: 0px 10px 20px rgba(101, 41, 255, 0.15);
-    border-radius: 30px;
-    justify-self: center;
-    transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+const SayingHello = styled.p`
+    @import url('https://fonts.googleapis.com/css?family=Lato:100,300,400,700');	font-size: 28px;
+    font-weight: 700;
+    color: #F5FFFF;
+    font-size: 32px;
+    font-family: 'Lato', sans-serif;
+`
 
-    &:hover {
-        box-shadow: 0 20px 40px rgba(0,0,0, 0.15);
-        transform: translateY(-3px);
-    }
+const SayingHelloSubText = styled.p`
+    @import url('https://fonts.googleapis.com/css?family=Lato:100,300,400,700');    font-size: 28px;
+    font-family: 'Lato', sans-serif;
+    font-weight: 400;
+    font-size: 18px;
+    color: #757B7F;
+    margin-top: 0px;
 `
 
 const LinkGroup = styled.div`
@@ -46,34 +44,45 @@ const LinkGroup = styled.div`
 	display: grid;
 	grid-template-columns: repeat(2, 1fr);
 	grid-gap: 10px;
+    color: #F5FFFF;
 
     a{
+        transition: 0.8s;
+        color: #F5FFFF;
+    }
+
+    a:hover {
+        cursor: pointer;
+        color: #8AB9CD;
+    }
+`
+const Copyright = styled.div`
+    color: #F5FFFF;
+    max-width: 500px;
+    margin: 0 auto;
+    padding: 0 20px;
+
+    a{
+        color: #F5FFFF;
         transition: 0.8s;
     }
 
     a:hover {
         cursor: pointer;
-        color: black;
+        color: #FFFFFF;
+        transform: translateY(-3px);
     }
-`
-const Copyright = styled.div`
-    color: #1F1F1F;
-    max-width: 500px;
-    margin: 0 auto;
-    padding: 0 20px;
-`
 
+`
 
 const Footer = ({data, children}) => (
         <FooterGroup>
-        	<Text>
-        	Founder and pusher of pixels at Veve, offically launching 2019.
-        	</Text>
-        	<SecondaryText>
-			Say Hello 🤙
-        	</SecondaryText>
-        	<Text>For work enquires head over to Veve.</Text>
-        	<Button>Medium</Button>
+            <Saying>
+                <SayingHello>
+                Say Hello 🤙
+                </SayingHello>
+                <SayingHelloSubText>For work enquires head over to Veve.</SayingHelloSubText>
+            </Saying>
         	<LinkGroup>
 				{data.allContentfulLink.edges.map(edge => (
 				          <a href={edge.node.url}>{edge.node.title}</a>
---
+++ src/pages/Photos.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+import styled from 'styled-components'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+
+const PhotosPage = () => (
+	<Layout>
+
+	</Layout>
+)
+
+export default PhotosPage
