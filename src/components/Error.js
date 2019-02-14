45e7435f9ad6b985f5dd1d7c29dda66127514fb3
trujamal <16698402+trujamal@users.noreply.github.com>
2019-02-13 23:00:58 -0600

    Updates to error 404 page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,1 +1,14 @@
-Error.js
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Error404Page = () => (
+	<Layout>
+    <SEO title="How did you get here?" keywords={[`gatsby`, `application`, `react`]} />
+
+	</Layout>
+)
+
+export default Error404Page
