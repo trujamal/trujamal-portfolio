16f1c3f8340f209b360fa64a825479e7e7bd3426
trujamal <16698402+trujamal@users.noreply.github.com>
2019-03-04 01:06:31 -0600

    Updates to portfolio

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -6,17 +6,35 @@ import HomepageBanner from '../components/HomepageBanner'
 import Cell from '../components/Cell'
 import photosdata from '../../json_data/photosdata.json'
 
+const ContainerGroup = styled.div`
+  margin: 0 auto;
+  max-width: 1200px;
+  margin: 0 auto 5vh;
+`
 
 const SectionCellGroup = styled.div`
+  clear: left;
   padding-top: 15vh;
-  max-width: 1200px;
   column-count: 3;
   column-gap: 20px;
-  padding: 20px;
-  margin: 0 auto 5vh;
+  padding: 20px 0px;
   @media (max-width: 800px) {
     column-count: 2;
   }
+`
+const SectionNavGroup = styled.nav`
+  padding-bottom: 40px;
+  float: left;
+  width: 208px;
+  display: block;  
+`
+const NavSectionTags = styled.a`
+  position: relative;
+  color: #1A1B1C;
+  margin-right: 16px;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 24px;
 `
 
 
@@ -27,12 +45,20 @@ const PhotosPage = () => (
       title="Photography" 
       text="I work on extending my skillset, by containing a natural eye for the enviornment and people as a whole. Here is a look into some of the works that I've shot and edited." 
     />
-    <SectionCellGroup>
-    {photosdata.cells.map(cell => (
-            <Cell 
-              image={cell.image}/>
-          ))}
-    </SectionCellGroup>
+    <ContainerGroup>
+      <SectionNavGroup>
+        <NavSectionTags>Portraits</NavSectionTags>
+        <NavSectionTags>Travel</NavSectionTags>
+        <NavSectionTags>Urban</NavSectionTags>
+      </SectionNavGroup>
+      <SectionCellGroup>
+      {photosdata.cells.map(cell => (
+              <Cell 
+                image={cell.image}/>
+            ))}
+      </SectionCellGroup>
+    </ContainerGroup>
+
 	</Layout>
 )
 
